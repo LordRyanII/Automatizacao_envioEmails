9e9c366c50724ba0c3982497a14408fdad4ae3a5
LordRyanII <ryanoliveiram2020@outlook.com>
2023-10-02 17:34:29 -0300

    Feat: Possibilitando envio de texto para vários contatos

--- backend/src/services/sendEmail.js
+++ backend/src/services/sendEmail.js
@@ -9,21 +9,30 @@ const transporter = nodemailer.createTransport({
     }
 });
 
-// async..await is not allowed in global scope, must use a wrapper
 const main = async (body) => {
     const { from, to, subject, text } = body;
-    try {
-        const options = await transporter.sendMail({
-            from: '<' + from + '>', // Quem envia
-            to: to, // Quem recebe
-            subject: subject, // Assunto
-            text: text, // Texto
-        });
-        return "Message enviada: %s";
-    } catch (err) {
-        return err;
+    
+    if (typeof to !== 'string') {
+        throw new Error('O campo "to" deve ser uma string contendo emails separados por vírgulas.');
     }
-
-
+    
+    const destinatarios = to.split(',').map(email => email.trim());
+    
+    for (let i = 0; i < destinatarios.length; i++) {
+        const destinatario = destinatarios[i];
+        
+        try {
+            const options = await transporter.sendMail({
+                from: `<${from}>`, // Quem envia
+                to: destinatario, // Quem recebe
+                subject: subject, // Assunto
+                text: text, // Texto
+            });
+            console.log(`Email enviado para ${destinatario}.`);
+        } catch (err) {
+            console.error(`Erro ao enviar email para ${destinatario}: ${err}`);
+        }
+    }
 }
-module.exports = main
+
+module.exports = main;
